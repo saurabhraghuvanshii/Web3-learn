18040fc993c6f09428e7460e8ce318d47d0a0bd9
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2025-08-04 19:23:21 +0000

    traits

--- week1/traits-macros/src/main.rs
+++ week1/traits-macros/src/main.rs
@@ -1,19 +1,50 @@
-struct User {
-    name: String,
-    age: u32
+// struct User {
+//     name: String,
+//     age: u32
+// }
+
+// // for printing User without this no Printing using println!
+// impl std::fmt::Display for User {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         write!(f, "name is {}, age is {})", self.name, self.age)
+//     }
+// }
+
+// fn main() {
+//     let u = User{
+//         name: String::from("Saurabh"),
+//         age:18
+//     };
+//     println!("{}", u);
+// }
+
+
+// traits are similar to interfaces in Java/Js
+
+trait Shape {
+    fn area(&self) -> f32;
 }
 
-// for printing User without this no Printing using println!
-impl std::fmt::Display for User {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "name is {}, age is {})", self.name, self.age)
+// Struct is used to implement traits
+
+struct Rect {
+    width: f32,
+    height: f32,
+}
+
+impl Shape for Rect {
+    fn area(&self) -> f32 {
+        return self.width * self.height;
     }
 }
 
-fn main() {
-    let u = User{
-        name: String::from("Saurabh"),
-        age:18
-    };
-    println!("{}", u);
+// functions that accepts any type implementing Shape
+fn get_area(shape: impl Shape) -> f32 {
+    return shape.area()
+}
+
+fn main () {
+    let rect = Rect {width: 10.0, height: 5.0 };
+    let area = get_area(rect);
+    println!("Area of rect {}", area);
 }
